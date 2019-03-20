45a0ba49727265629a668f2c2e512be5160208a6
cogud0908 <cogud0908@naver.com>
2019-03-20 15:15:04 +0900

    dafsfdsa

--- src/main/java/com/douzone/guestbook/config/WebConfig.java
+++ src/main/java/com/douzone/guestbook/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.douzone.emaillist.config;
+package com.douzone.guestbook.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
