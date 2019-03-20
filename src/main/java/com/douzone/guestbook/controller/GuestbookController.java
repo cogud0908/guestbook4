5e303e3d522d6e0ec284c8345ec3a4da2ba41ea3
cogud0908 <cogud0908@naver.com>
2019-03-20 14:46:13 +0900

    init commit

---
+++ src/main/java/com/douzone/guestbook/controller/GuestbookController.java
@@ -0,0 +1,46 @@
+package com.douzone.guestbook.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.douzone.guestbook.dao.GuestbookDao;
+import com.douzone.guestbook.vo.GuestbookVo;
+
+@Controller
+public class GuestbookController {
+	
+	@Autowired
+	private GuestbookDao dao;
+	
+	@RequestMapping({"","/list"})
+	public String list(Model model) {
+		
+		model.addAttribute("list", dao.getList());
+				
+		return "list";
+	}
+	
+	@RequestMapping(value="/add", method=RequestMethod.POST)
+	public String add(GuestbookVo vo) {
+		dao.insert(vo);
+		
+		return "redirect:/";
+	}
+	
+	@RequestMapping(value="/delete", method=RequestMethod.GET)
+	public String deleteform(@RequestParam(value="no", required=false) Long no, Model model) {
+		model.addAttribute("no", no);
+		return "deleteform";
+	}
+	
+	@RequestMapping(value="/delete", method=RequestMethod.POST)
+	public String delete(GuestbookVo vo) {
+		dao.delete(vo);
+		
+		return "redirect:/";
+	}
+}
